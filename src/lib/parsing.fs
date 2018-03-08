0fa444f7d8502bff5673362cc65abb8e78fb20a9
programandala.net <github@programandala.net>
2018-03-08 22:30:47 +0000

    Update library to Solo Forth 0.14.0-pre.464
    FossilOrigin-Name: 2915b7020e88ee4ec0139ea3e2d8c8095ebf436ab2f63661a0ac74f0f33e525d

--- src/lib/parsing.fs
+++ src/lib/parsing.fs
@@ -3,7 +3,7 @@
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ Last modified: 201803052149
+  \ Last modified: 201803081227
   \ See change log at the end of the file
 
   \ ===========================================================
@@ -27,18 +27,37 @@
 
 unneeding defined?
 
-?\ : defined? ( ca len -- f ) undefined? 0= ;
+?\ : defined? ( ca len -- f ) find-name 0<> ;
+
+  \ doc{
+  \
+  \ defined? ( ca len -- f ) "defined-question"
+  \
+  \ Find name _ca len_.  If the definition is
+  \ found after searching the active search
+  \ order, return true, else return false.
+  \
+  \ See: `undefined?`, `defined`, `find-name`.
+  \
+  \ }doc
 
 unneeding parse-char
 
-?\ : parse-char ( "c"  -- c ) stream drop c@ 1 parsed ;
-  \ Parse the next char in the input stream and return its
-  \ code.
+?\ : parse-char ( "c" -- c ) stream drop c@ 1 parsed ;
 
-unneeding parse-all ?(
+  \ doc{
+  \
+  \ parse-char ( "c" -- c )
+  \
+  \ Parse the next character in the input stream and return its
+  \ code _c_.
+  \
+  \ See: `parse-name`, `parse`, `parsed`, `stream`.
+  \
+  \ }doc
 
-: parse-all ( "ccc" -- ca len )
-  stream dup parsed >stringer ; ?)
+unneeding parse-all ?( : parse-all ( "ccc" -- ca len )
+                         stream dup parsed >stringer ; ?)
 
   \ doc{
   \
@@ -77,7 +96,7 @@ unneeding string>source ?(
 
   \ doc{
   \
-  \ string>source ( ca len -- )
+  \ string>source ( ca len -- ) "string-to-source"
   \
   \ Set the string _ca len_ as the current source.
   \
@@ -158,6 +177,9 @@ unneeding [char]  ?(
   \ doc{
   \
   \ [char]
+  \   Compilation: ( "name" -- )
+  \   Run-time: ( -- c )
+  \ "bracket-char"
   \
   \ Compilation: ( "name" -- )
   \
@@ -291,5 +313,9 @@ unneeding word ?(
   \ 2017-04-27: Fix needing of `[char]`.
   \
   \ 2018-03-05: Update `[unneeded]` to `unneeding`.
+  \
+  \ 2018-03-07: Add words' pronunciaton.
+  \
+  \ 2018-03-08: Documente `defined?` and `parse-char`.
 
   \ vim: filetype=soloforth
